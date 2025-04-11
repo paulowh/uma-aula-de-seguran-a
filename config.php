610cc1ccaf8f4c5fc3b0b34e6adfe794e6948f65
paulowh <paulo.whsantos@hotmail.com>
2025-04-11 11:04:57 -0300

    arquivo de configuração

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn['host']    = '185.213.81.205'
+$conn['banco']   = 'db_cinebox';
+$conn['usuario'] = 'u336727971_hostinger';
+$conn['senha']   = 'DreamGame@1';
+
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-
-$host = '185.213.81.205'
-$banco = 'db_cinebox';
-$usuario = 'u336727971_hostinger';
-$senha = 'DreamGame@1';
+require './config.php';
 
 
-$conexao = '';
-
-var_dump($conexao);
+var_dump($conn);
